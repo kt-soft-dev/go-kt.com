e1c6ebbabaeabee385dde7bc728dd702be71c60d
Borys Stelmakh <bs@vortalcoil.com>
2024-04-29 16:00:41 +0400

    Cleaned up some bad files and variables

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -7,17 +7,19 @@ import { createI18n } from 'vue-i18n';
 export type MessageLanguages = keyof typeof messages;
 export type MessageSchema = (typeof messages)['ruXs'];
 
-// See https://vue-i18n.intlify.dev/guide/advanced/typescript.html#global-resource-schema-type-definition
 /* eslint-disable @typescript-eslint/no-empty-interface */
 declare module 'vue-i18n' {
   // noinspection JSUnusedGlobalSymbols
-  export interface DefineLocaleMessage extends MessageSchema {}
+  export interface DefineLocaleMessage extends MessageSchema {
+  }
 
   // noinspection JSUnusedGlobalSymbols
-  export interface DefineDateTimeFormat {}
+  export interface DefineDateTimeFormat {
+  }
 
   // noinspection JSUnusedGlobalSymbols
-  export interface DefineNumberFormat {}
+  export interface DefineNumberFormat {
+  }
 }
 
 export function getAdaptiveLocale(): MessageLanguages {
@@ -29,7 +31,7 @@ export default boot(({ app }) => {
   const i18n = createI18n({
     locale: getAdaptiveLocale(),
     legacy: false,
-    messages,
+    messages
   });
   app.use(i18n);
 });
